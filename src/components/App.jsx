35b9d093b8498dff5c42083fd4a9067d9f9b36fd
Your Name <pranesh15shrestha@gmail.com>
2025-12-19 12:13:11 +0545

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+function App() {
+  const [item, setItem] = React.useState([]);
+  const [input, setInput] = React.useState("");
+
+  function addItem(event) {
+    const iName = event.target.value;
+    setInput(iName);
+  }
+
+  function addInput() {
+    setItem((prevItem) => {
+      return [...prevItem, input];
+    });
+    setInput("");
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      addInput();
+    }
+  }
+
+  function clearList() {
+    setItem([]);
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input
+          onChange={addItem}
+          type="text"
+          name="itemname"
+          value={input}
+          onKeyDown={handleKeyPress}
+        />
+        <button>
+          <span onClick={addInput}>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {item.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="clear-wrapper">
+        <button className="clearBut" onClick={clearList}>
+          <span>clear</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
